11436734305025b45930782dac6dfbb449c122be
Vera Nikalaikina <nikolajkina@gmail.com>
2020-02-24 15:38:31 +0300

    Cats core exercises

--- src/main/scala/com/evolutiongaming/bootcamp/cats/TypeHierarchy.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/cats/TypeHierarchy.scala
@@ -1,5 +1,7 @@
 package com.evolutiongaming.bootcamp.cats
 
+import cats.Functor
+
 object TypeHierarchy {
 
   /*
@@ -42,4 +44,34 @@ object TypeHierarchy {
 
 
   // Laws
+}
+
+object Excercises {
+  trait Applicative[F[_]] extends Functor[F] {
+    def map[A,B](fa: F[A])(f: A => B): F[B]
+
+    def unit[A](a: => A): F[A]
+
+    // implement methods using other methods
+    def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
+
+    def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
+
+    def sequence[A](fas: List[F[A]]): F[List[A]] = ???
+
+    def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] = ???
+  }
+
+  trait Monad[M[_]] extends Functor[M] {
+    def unit[A](a: => A): M[A]
+
+    // implement methods using other methods
+    def flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+
+    def join[A](mma: M[M[A]]): M[A] = ???
+
+    def map[A,B](ma: M[A])(f: A => B): M[B] = ???
+
+    def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] = ???
+  }
 }
