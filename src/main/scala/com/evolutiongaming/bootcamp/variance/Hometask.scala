714540ad54dd576d770346a81a4e9cac80bd6e23
Andrey Bobylev <abobylev@evolutiongaming.com>
2020-03-03 15:04:40 +0300

    Fixes

--- src/main/scala/com/evolutiongaming/bootcamp/variance/Hometask.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/variance/Hometask.scala
@@ -19,12 +19,13 @@ object Hometask extends App {
   }
 
   sealed abstract case class Matrix[+A] private (matrix: Vector[Vector[A]], focus: Focus = Focus.Zero) {
-    def get(x: Int, y: Int): Option[A] = matrix.get(x).flatMap(_.get(y))
+    def get(x: Int, y: Int): Option[A] = matrix.get(y).flatMap(_.get(x))
   }
 
   object Matrix {
 
     def of[A](data: Iterable[A], rows: Int): Either[String, Matrix[A]] = for {
+      _ <- Either.cond(data.nonEmpty, (), "Data should not be empty")
       _ <- Either.cond(rows > 0, (), "Rows count should be positive")
       _ <- Either.cond(data.size % rows == 0, (), "Every row must contain the same number of elements")
     } yield new Matrix[A](data.sliding(data.size / rows, data.size / rows).map(_.toVector).toVector) {}
@@ -50,8 +51,8 @@ object Hometask extends App {
   private def readImage(file: String): Image = {
     val image = ImageIO.read(getClass.getResourceAsStream(file))
     val data  = for {
-      x <- 0 until image.getHeight
-      y <- 0 until image.getWidth
+      y <- 0 until image.getHeight
+      x <- 0 until image.getWidth
     } yield toRGB(image.getRGB(x, y))
     Matrix.of(data, image.getHeight).getOrElse(sys.error("Failed to read image"))
   }
@@ -59,25 +60,25 @@ object Hometask extends App {
   private def writeImage(data: Image, file: String): Unit = {
     val image = new BufferedImage(data.matrix(0).size, data.matrix.size, BufferedImage.TYPE_INT_RGB)
     for {
-      x   <- data.matrix.indices
-      y   <- data.matrix(0).indices
-      rgb =  data.matrix(x)(y)
-    } image.setRGB(x, y, rgb._1 << 16 | rgb._2 << 8 | rgb._3)
+      x         <- data.matrix(0).indices
+      y         <- data.matrix.indices
+      (r, g, b) <- data.get(x, y)
+    } image.setRGB(x, y, r << 16 | g << 8 | b)
     ImageIO.write(image, "bmp", new File("src/main/resources", file))
   }
 
   private def smooth(radius: Int)(image: Image): RGB = {
-    val points = (-radius / 2 until radius / 2).toList
-    val sum = (points, points).mapN { case (x, y) =>
+    val points    = (-radius / 2 until radius / 2).toList
+    val (r, g, b) = (points, points).mapN { case (x, y) =>
       image.get(image.focus.x + x, image.focus.y + y).getOrElse((0xFF, 0xFF, 0xFF))
     }.combineAll
     val sqr = radius * radius
-    (sum._1 / sqr, sum._2 / sqr, sum._3 / sqr)
+    (r / sqr, g / sqr, b / sqr)
   }
 
   private def mirror(image: Image): RGB = {
-    val mirror = (image.matrix(0).size - 1) - image.focus.x
-    image.matrix(mirror)(image.focus.y)
+    val mirrored = (image.matrix(0).size - 1) - image.focus.x
+    image.matrix(image.focus.y)(mirrored)
   }
 
   val kitty    = readImage("/cat.jpg")
