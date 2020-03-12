ecfa97bd9eaec1043b94da7d4e1705be041bb7af
Evgeny Kurnevsky <kurnevsky@gmail.com>
2020-03-12 12:59:53 +0300

    Use App for akka server.

--- src/main/scala/com/evolutiongaming/bootcamp/http/AkkaServer.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/http/AkkaServer.scala
@@ -12,128 +12,126 @@ import scala.concurrent.Await
 import scala.concurrent.duration._
 import scala.io.StdIn
 
-object AkkaServer {
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("my-system")
-    implicit val executionContext = system.dispatcher
+object AkkaServer extends App {
+  implicit val system = ActorSystem("my-system")
+  implicit val executionContext = system.dispatcher
 
-    val helloRoute =
-      path("hello" / Segment) { name =>
-        get {
-          // curl 'localhost:9000/hello/world'
+  val helloRoute =
+    path("hello" / Segment) { name =>
+      get {
+        // curl 'localhost:9000/hello/world'
+        complete(s"Hello $name!")
+      }
+    } ~ path("hello") {
+      post {
+        entity(as[String]) { name =>
+          // curl -XPOST 'localhost:9000/hello' -d 'world'
           complete(s"Hello $name!")
         }
-      } ~ path("hello") {
-        post {
-          entity(as[String]) { name =>
-            // curl -XPOST 'localhost:9000/hello' -d 'world'
-            complete(s"Hello $name!")
-          }
-        }
-      }
-
-    // Query parameters
-
-    val paramsRoute =
-      (get & pathPrefix("int")) {
-        path(IntNumber) { n =>
-          // curl 'localhost:9000/int/42'
-          complete(s"Passed: $n")
-        } ~ parameter(Symbol("val")) { n =>
-          // curl 'localhost:9000/int?val=42'
-          complete(s"Passed: $n")
-        }
-      }
-
-    // Headers/cookies
-
-    val headersRoute = get {
-      path("headers") {
-        headerValuePF {
-          case HttpHeader("request-header", v) => v
-        } { header =>
-          respondWithHeader(RawHeader("Response-Header", "response value")) {
-            // curl -v 'localhost:9000/headers' -H 'Request-Header: request value'
-            complete(header)
-          }
-        }
-      } ~ path("cookies") {
-        cookie("request-cookie") { cookie =>
-          setCookie(HttpCookie("response-cookie", value = "response_value")) {
-            // curl 'localhost:9000/cookies' -b "request-cookie=request_value"
-            complete(cookie.value)
-          }
-        }
       }
     }
 
-    // Body encoding/decoding
+  // Query parameters
 
-    val entityRoute = {
-      final case class Hello(name: String)
-
-      val NameRegex = """\((.*)\)""".r
-      implicit val helloUnmarshaller = Unmarshaller.stringUnmarshaller.map {
-        case NameRegex(s) => Hello(s)
-        case s => throw new IllegalArgumentException(s"Invalid value: $s")
-      }
-
-      (post & path("entity")) {
-        post {
-          entity(as[Hello]) { hello =>
-            // curl -XPOST 'localhost:9000/hello' -d 'world'
-            complete(s"Hello ${hello.name}!")
-          }
-        }
+  val paramsRoute =
+    (get & pathPrefix("int")) {
+      path(IntNumber) { n =>
+        // curl 'localhost:9000/int/42'
+        complete(s"Passed: $n")
+      } ~ parameter(Symbol("val")) { n =>
+        // curl 'localhost:9000/int?val=42'
+        complete(s"Passed: $n")
       }
     }
 
-    // Body json encoding/decoding
+  // Headers/cookies
 
-    val jsonRoute = {
-      final case class Hello(name: String)
+  val headersRoute = get {
+    path("headers") {
+      headerValuePF {
+        case HttpHeader("request-header", v) => v
+      } { header =>
+        respondWithHeader(RawHeader("Response-Header", "response value")) {
+          // curl -v 'localhost:9000/headers' -H 'Request-Header: request value'
+          complete(header)
+          }
+      }
+    } ~ path("cookies") {
+      cookie("request-cookie") { cookie =>
+        setCookie(HttpCookie("response-cookie", value = "response_value")) {
+          // curl 'localhost:9000/cookies' -b "request-cookie=request_value"
+          complete(cookie.value)
+        }
+      }
+    }
+  }
 
-      import io.circe.generic.auto._
-      import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
+  // Body encoding/decoding
 
-      // implicit val helloUnmarshaller = FailFastCirceSupport.unmarshaller[Hello]
+  val entityRoute = {
+    final case class Hello(name: String)
 
-      (post & path("json")) {
+    val NameRegex = """\((.*)\)""".r
+    implicit val helloUnmarshaller = Unmarshaller.stringUnmarshaller.map {
+      case NameRegex(s) => Hello(s)
+      case s => throw new IllegalArgumentException(s"Invalid value: $s")
+    }
+
+    (post & path("entity")) {
+      post {
         entity(as[Hello]) { hello =>
-          // curl -XPOST 'localhost:9000/json' -d '{"name": "world"}' -H "Content-Type: application/json"
+          // curl -XPOST 'localhost:9000/hello' -d 'world'
           complete(s"Hello ${hello.name}!")
         }
       }
     }
+  }
 
-    // WebSockets
+  // Body json encoding/decoding
 
-    val wsEchoRoute = path("wsecho") {
-      val flow = Flow[Message].collect {
-        case TextMessage.Strict(msg) => TextMessage.Strict(msg)
-      }
+  val jsonRoute = {
+    final case class Hello(name: String)
 
-      // websocat 'ws://127.0.0.1:9000/wsecho'
-      handleWebSocketMessages(flow)
-    }
+    import io.circe.generic.auto._
+    import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 
-    // Multipart
+    // implicit val helloUnmarshaller = FailFastCirceSupport.unmarshaller[Hello]
 
-    val multipartRoute = (post & path("multipart")) {
-      entity(as[Multipart.FormData]) { m =>
-        // curl -XPOST 'localhost:9000/multipart' -F 'text=request value' -F file=@file.txt
-        complete(m.parts.map(_.name).runWith(Sink.seq).map(_.mkString("\n")))
+    (post & path("json")) {
+      entity(as[Hello]) { hello =>
+        // curl -XPOST 'localhost:9000/json' -d '{"name": "world"}' -H "Content-Type: application/json"
+        complete(s"Hello ${hello.name}!")
       }
     }
+  }
 
-    val route = helloRoute ~ paramsRoute ~ headersRoute ~ entityRoute ~ jsonRoute ~ multipartRoute ~ wsEchoRoute
+  // WebSockets
 
-    val binding = Http().bindAndHandle(route, "localhost", 9000)
+  val wsEchoRoute = path("wsecho") {
+    val flow = Flow[Message].collect {
+      case TextMessage.Strict(msg) => TextMessage.Strict(msg)
+    }
 
-    sys.addShutdownHook {
-      binding.flatMap(_.terminate(hardDeadline = 3.seconds)).flatMap { _ =>
-        system.terminate()
-      }
+    // websocat 'ws://127.0.0.1:9000/wsecho'
+    handleWebSocketMessages(flow)
+  }
+
+  // Multipart
+
+  val multipartRoute = (post & path("multipart")) {
+    entity(as[Multipart.FormData]) { m =>
+      // curl -XPOST 'localhost:9000/multipart' -F 'text=request value' -F file=@file.txt
+      complete(m.parts.map(_.name).runWith(Sink.seq).map(_.mkString("\n")))
+    }
+  }
+
+  val route = helloRoute ~ paramsRoute ~ headersRoute ~ entityRoute ~ jsonRoute ~ multipartRoute ~ wsEchoRoute
+
+  val binding = Http().bindAndHandle(route, "localhost", 9000)
+
+  sys.addShutdownHook {
+    binding.flatMap(_.terminate(hardDeadline = 3.seconds)).flatMap { _ =>
+      system.terminate()
     }
   }
 }
