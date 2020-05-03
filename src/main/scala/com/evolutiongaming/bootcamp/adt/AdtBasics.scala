3674759b15793b7e37fbbaed0406c57edb2e299a
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-05-03 20:58:50 +0300

    Add AdtBasics

---
+++ src/main/scala/com/evolutiongaming/bootcamp/adt/AdtBasics.scala
@@ -0,0 +1,158 @@
+package com.evolutiongaming.bootcamp.adt
+
+object AdtBasics {
+
+  // INTRO
+
+  // ADTs stand for Algebraic Data Types. This is a commonly used way of structuring data, used in many
+  // programming languages (so this is not something unique to Scala).
+  //
+  // While the definition may sound scientific and complex, in reality we have already been using this
+  // concept in `basics` package. For example, see `sealed trait Shape` in `ClassesAndTraits`. We will now
+  // look into the concept and its use cases in more detail.
+
+  // ADTs are widely used in Scala for multiple reasons:
+  // - to ensure it is hard or even impossible to represent invalid data;
+  // - because pattern matching and ADTs play nicely together.
+
+  // Two common classes of ADTs are:
+  // 1. product types: case classes and tuples;
+  // 2. sum types: sealed traits and abstract classes.
+
+  // PRODUCT TYPES
+
+  // A product type allows to combine multiple values into one. Canonical Scala examples of product types are
+  // case classes and tuples. See `Basics` and `ClassesAndTraits` for their introduction.
+
+  // A product type is called like that because one can calculate how many different values it can possibly
+  // have by multiplying the number of such possibilities for the types it combines. The resulting number
+  // is called the arity of the product type.
+
+  // Question. What is the arity of the product type `(Boolean, Boolean)`?
+  type DoubleBoolean = (Boolean, Boolean)
+
+  // Question. What is the arity of the product type `Person`?
+  final case class Person(name: String, surname: String, age: Int)
+
+  // Question. `Int`, `Double`, `String`, etc. are useful types from the Scala standard library, which can
+  // represent a wide range of data. In the product type `Person`, both the name and the surname are
+  // represented by `String`. Is that a good idea?
+
+  // VALUE CLASSES
+
+  // Value classes are a mechanism in Scala to avoid allocating runtime objects, while still providing
+  // additional type safety. Runtime objects are not allocated in most cases, but there are notable
+  // exceptions, see the following link for more details:
+  // https://docs.scala-lang.org/overviews/core/value-classes.html
+
+  // `Age` has a single, public val parameter that is the underlying runtime representation. The type at
+  // compile time is `Age`, but at runtime, the representation is `Int`. Case classes can also be used to
+  // define value classes, see `Name`.
+  class Age(val value: Int) extends AnyVal
+  final case class Name(value: String) extends AnyVal
+
+  // Type aliases may seem similar to value classes, but they provide no additional type safety. They can,
+  // however, increase readability of the code in certain scenarios.
+  final case class Surname(value: String) extends AnyVal
+  type SurnameAlias = String // No additional type safety in comparison to `String`, arguably a bad example!
+
+  // Question. Can you come up with an example, where using type aliases would make sense?
+
+  // Exercise. Rewrite the product type `Person`, so that it uses value classes.
+
+  // SMART CONSTRUCTORS
+
+  // Smart constructor is a pattern, which allows creating only valid instances of a class.
+
+  // Exercise. Create a smart constructor for `GameLevel` that only permits levels from 1 to 80.
+  final case class GameLevel private (value: Int) extends AnyVal
+  object GameLevel {
+    def create(value: Int): Option[GameLevel] = ???
+  }
+
+  // To disable creating case classes in any other way besides smart constructor, the following pattern
+  // can be used. However, it is rather syntax-heavy and cannot be combined with value classes.
+  // Exercise. Implement the smart constructor for `Time` that only permits values from 00:00 to 23:59 and
+  // returns "Invalid hour value" or "Invalid minute value" strings in `Left` when appropriate.
+  // Question. Is using `String` to represent `Left` a good idea?
+  sealed abstract case class Time private (hour: Int, minute: Int)
+  object Time {
+    def create(hour: Int, minute: Int): Either[String, Time] = Right(new Time(hour, minute) {})
+  }
+
+  // SUM TYPES
+
+  // A sum type is an enumerated type. To define it one needs to enumerate all its possible variants.
+  // A custom boolean type `Bool` can serve as a canonical example.
+  sealed trait Bool
+  object Bool {
+    final case object True extends Bool
+    final case object False extends Bool
+  }
+
+  // Note that sealed keyword means that `Bool` can only be extended in the same file as its declaration.
+  // Question. Why do you think sealed keyword is essential to define sum types?
+
+  // A sum type is called like that because one can calculate how many different values it can possibly
+  // have by adding the number of such possibilities for the types it enumerates. The resulting number
+  // is called the arity of the sum type.
+
+  // Question. What is the arity of the sum type `Bool`?
+
+  // The power of sum and product types is unleashed when they are combined together. For example, consider a
+  // case where multiple different payment methods need to be supported. (This is an illustrative example and
+  // should not be considered complete.)
+  final case class AccountNumber(value: String) extends AnyVal
+  final case class CardNumber(value: String) extends AnyVal
+  final case class ValidityDate(month: Int, year: Int)
+  sealed trait PaymentMethod
+  object PaymentMethod {
+    final case class BankAccount(accountNumber: AccountNumber) extends PaymentMethod
+    final case class CreditCard(cardNumber: CardNumber, validityDate: ValidityDate) extends PaymentMethod
+    final case object Cash extends PaymentMethod
+  }
+
+  import PaymentMethod._
+
+  final case class PaymentStatus(value: String) extends AnyVal
+  trait BankAccountService {
+    def processPayment(amount: BigDecimal, accountNumber: AccountNumber): PaymentStatus
+  }
+  trait CreditCardService {
+    def processPayment(amount: BigDecimal, cardNumber: CreditCard): PaymentStatus
+  }
+  trait CashService {
+    def processPayment(amount: BigDecimal): PaymentStatus
+  }
+
+  // Exercise. Implement `PaymentService.processPayment` using pattern matching and ADTs.
+  class PaymentService(
+    bankAccountService: BankAccountService,
+    creditCardService: CreditCardService,
+    cashService: CashService,
+  ) {
+    def processPayment(amount: BigDecimal, method: PaymentMethod): PaymentStatus = ???
+  }
+
+  // Let's compare that to `NaivePaymentService.processPayment` implementation, which does not use ADTs, but
+  // provides roughly the same features as `PaymentService`.
+  // Question. What are disadvantages of `NaivePaymentService`? Are there any advantages?
+  trait NaivePaymentService { // Obviously a bad example!
+    def processPayment(
+      amount: BigDecimal,
+      bankAccountNumber: Option[String],
+      validCreditCardNumber: Option[String],
+      isCash: Boolean,
+    ): String = ???
+  }
+
+  // Exercise. Define an algebraic data type `Car`, which has a manufacturer, a model, a production year,
+  // and a license plate number (can contain from 3 to 8 upper case letters and numbers). Use value classes
+  // and smart constructors as appropriate.
+  type Car = Nothing
+
+  // Attributions and useful links:
+  // https://nrinaudo.github.io/scala-best-practices/definitions/adt.html
+  // https://alvinalexander.com/scala/fp-book/algebraic-data-types-adts-in-scala/
+  // https://en.wikipedia.org/wiki/Algebraic_data_type
+}
---
+++ src/test/scala/com/evolutiongaming/bootcamp/adt/AdtBasicsSpec.scala
@@ -0,0 +1,87 @@
+package com.evolutiongaming.bootcamp.adt
+
+import com.evolutiongaming.bootcamp.adt.AdtBasics.PaymentMethod._
+import com.evolutiongaming.bootcamp.adt.AdtBasics._
+import org.scalacheck.Gen._
+import org.scalatest.{EitherValues, OptionValues}
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
+
+class AdtBasicsSpec
+  extends AnyFlatSpec
+    with OptionValues
+    with EitherValues
+    with ScalaCheckDrivenPropertyChecks {
+
+  "GameLevel" should "allow being created with levels between 1 and 80" in {
+    forAll(choose(min = 1, max = 80)) { v: Int =>
+      GameLevel.create(v).value.value shouldBe v
+    }
+  }
+
+  it should "forbid being created with invalid levels" in {
+    forAll { v: Int =>
+      whenever(v < 1 || v > 80) {
+        GameLevel.create(v) shouldBe None
+      }
+    }
+  }
+
+  "Time" should "allow being created with valid hour and minute values" in {
+    forAll(choose(min = 0, max = 23), choose(min = 0, max = 59)) { (hour: Int, minute: Int) =>
+      val time = Time.create(hour, minute).right.value
+      time.hour shouldBe hour
+      time.minute shouldBe minute
+    }
+  }
+
+  it should "forbid being created with invalid hour values" in {
+    forAll { hour: Int =>
+      whenever(hour < 0 || hour > 23) {
+        Time.create(hour = hour, minute = 30).left.value shouldBe "Invalid hour value"
+      }
+    }
+  }
+
+  it should "forbid being created with invalid minute values" in {
+    forAll { minute: Int =>
+      whenever(minute < 0 || minute > 59) {
+        Time.create(hour = 12, minute = minute).left.value shouldBe "Invalid minute value"
+      }
+    }
+  }
+
+  "PaymentService" should "process payment via correct method" in {
+    val paymentService = new PaymentService(
+      new TestBankAccountService,
+      new TestCreditCardService,
+      new TestCashService,
+    )
+    paymentService.processPayment(
+      amount = 12.34,
+      method = BankAccount(AccountNumber("123ABC")),
+    ) shouldBe PaymentStatus("Sent 12.34 to account number 123ABC")
+    paymentService.processPayment(
+      amount = 43.21,
+      method = CreditCard(CardNumber("123...53"), ValidityDate(12, 21)),
+    ) shouldBe PaymentStatus("Sent 43.21 to card number 123...53")
+    paymentService.processPayment(
+      amount = 100,
+      method = Cash,
+    ) shouldBe PaymentStatus("Sent 100 in cash")
+  }
+
+  private class TestBankAccountService extends BankAccountService {
+    override def processPayment(amount: BigDecimal, accountNumber: AccountNumber): PaymentStatus =
+      PaymentStatus(s"Sent $amount to account number ${accountNumber.value}")
+  }
+  private class TestCreditCardService extends CreditCardService {
+    override def processPayment(amount: BigDecimal, creditCard: CreditCard): PaymentStatus =
+      PaymentStatus(s"Sent $amount to card number ${creditCard.cardNumber.value}")
+  }
+  private class TestCashService extends CashService {
+    override def processPayment(amount: BigDecimal): PaymentStatus =
+      PaymentStatus(s"Sent $amount in cash")
+  }
+}
