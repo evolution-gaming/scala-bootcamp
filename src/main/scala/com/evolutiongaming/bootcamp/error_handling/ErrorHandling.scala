7d28aa7b3f1bf54c8e692ad587120905d8d9827e
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-09-28 11:23:23 +0300

    Polish

--- src/main/scala/com/evolutiongaming/bootcamp/error_handling/ErrorHandling.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/error_handling/ErrorHandling.scala
@@ -203,7 +203,7 @@ object ErrorHandling extends App {
 
   // Homework. Place the solution under `error_handling` package in your homework repository.
   //
-  // 1. Model `CreditCard` class as an ADT (protect against invalid data as much as makes sense).
+  // 1. Model `CreditCard` class as an ADT (protect against invalid data as much as it makes sense).
   // 2. Add `ValidationError` cases (at least 5, may be more).
   // 3. Implement `validate` method to construct `CreditCard` instance from the supplied raw data.
   object Homework {
