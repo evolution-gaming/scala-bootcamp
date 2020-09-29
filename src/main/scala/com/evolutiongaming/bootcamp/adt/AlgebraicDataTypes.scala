9e75c2dea67fad96a2f5a69b240d6af2bdebc5ea
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-09-29 13:59:06 +0300

    Improve ADT hometask

--- src/main/scala/com/evolutiongaming/bootcamp/adt/AlgebraicDataTypes.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/adt/AlgebraicDataTypes.scala
@@ -152,10 +152,21 @@ object AlgebraicDataTypes {
   // and smart constructors as appropriate.
   type Car = Nothing
 
-  // Homework. Define an algebraic data type `PokerHand`, which contains a collection of cards, representing a
-  // hand in a poker game. Make sure the defined model protects against invalid data. Use value classes and
-  // smart constructors as appropriate. Place the solution under `adt` package in your homework repository.
-  type PokerHand = Nothing
+  // Homework. Define all algebraic data types, which would be needed to implement “Hold’em Hand Strength”
+  // task you completed to join the bootcamp. Use your best judgement about particular data types to include
+  // in the solution, you can model concepts like:
+  //
+  // 1. Suite
+  // 2. Rank
+  // 3. Card
+  // 4. Hand (Texas or Omaha)
+  // 5. Board
+  // 6. Poker Combination (High Card, Pair, etc.)
+  // 7. Test Case (Board & Hands to rank)
+  // 8. Test Result (Hands ranked in a particular order)
+  //
+  // Make sure the defined model protects against invalid data. Use value classes and smart constructors as
+  // appropriate. Place the solution under `adt` package in your homework repository.
 
   // Attributions and useful links:
   // https://nrinaudo.github.io/scala-best-practices/definitions/adt.html
