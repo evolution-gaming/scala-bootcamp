c32addf457dbbaf19bfdd65272f96c6a4ab83938
Mikhail Sokolov <msokolov@evolutiongaming.com>
2020-10-08 18:07:18 +0300

    Extended exescises

--- src/main/scala/com/evolutiongaming/bootcamp/typeclass/Implicits.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/typeclass/Implicits.scala
@@ -321,7 +321,21 @@ object Implicits {
         ft.foldLeft(s)(f)
     }
 
+    case class Triple[T](
+      v1: T,
+      v2: T,
+      v3: T,
+    )
+
     /*
+    Part 1.
+
+    Define an Foldable instance for Triple (should behave like a collection of 3 elements)
+     */
+
+    /*
+    Part 2.
+
     Define another type-class - Summable[T] which should give us methods:
     - def plus(left: T, right: T): T
     - def zero: T
@@ -329,6 +343,10 @@ object Implicits {
     Define Summable[T] instances for:
     - any T which has the standard library Numeric[T] type-class provided
     - Set[S] - zero should be Set.empty and plus should merge sets with + operation
+     */
+
+    /*
+    Part 3.
 
     And finally - define generic collection sum method which works on any F[T]
     where F is Foldable (F[_]: Foldable) and T is Summable (T: Summable)!
