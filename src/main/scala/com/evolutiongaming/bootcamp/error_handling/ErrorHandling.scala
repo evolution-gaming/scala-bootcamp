847f28c495bb4d3c38f6442d4b9c582a80b4de2e
Andrejs Pavlovics <apavlovics2@evolutiongaming.com>
2020-10-14 10:19:08 +0300

    Rename CreditCard to PaymentCard

--- src/main/scala/com/evolutiongaming/bootcamp/error_handling/ErrorHandling.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/error_handling/ErrorHandling.scala
@@ -221,19 +221,19 @@ object ErrorHandling extends App {
 
   // Homework. Place the solution under `error_handling` package in your homework repository.
   //
-  // 1. Model `CreditCard` class as an ADT (protect against invalid data as much as it makes sense).
+  // 1. Model `PaymentCard` class as an ADT (protect against invalid data as much as it makes sense).
   // 2. Add `ValidationError` cases (at least 5, may be more).
-  // 3. Implement `validate` method to construct `CreditCard` instance from the supplied raw data.
+  // 3. Implement `validate` method to construct `PaymentCard` instance from the supplied raw data.
   object Homework {
 
-    case class CreditCard(/* Add parameters as needed */)
+    case class PaymentCard(/* Add parameters as needed */)
 
     sealed trait ValidationError
     object ValidationError {
       ??? // Add errors as needed
     }
 
-    object CreditCardValidator {
+    object PaymentCardValidator {
 
       type AllErrorsOr[A] = ValidatedNec[ValidationError, A]
 
@@ -242,7 +242,7 @@ object ErrorHandling extends App {
         number: String,
         expirationDate: String,
         securityCode: String,
-      ): AllErrorsOr[CreditCard] = ???
+      ): AllErrorsOr[PaymentCard] = ???
     }
   }
 
