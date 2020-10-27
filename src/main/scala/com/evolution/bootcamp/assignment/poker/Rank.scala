92f853293b35105d662718df27e8e0040da868dd
jurisk <jurisk@users.noreply.github.com>
2020-10-27 19:43:58 +0200

    Rank and Suit parsing

--- src/main/scala/com/evolution/bootcamp/assignment/poker/Rank.scala
+++ src/main/scala/com/evolution/bootcamp/assignment/poker/Rank.scala
@@ -1,8 +1,42 @@
 package com.evolution.bootcamp.assignment.poker
 
-sealed trait Rank
-// TODO
+sealed abstract class Rank private (val value: Char) {
+  override def toString: String = value.toString
+  def strength: Int = Rank.ordered.indexOf(this)
+}
 
 object Rank {
-  def of(x: Char): Either[ErrorMessage, Rank] = ???
+  case object Two extends Rank('2')
+  case object Three extends Rank('3')
+  case object Four extends Rank('4')
+  case object Five extends Rank('5')
+  case object Six extends Rank('6')
+  case object Seven extends Rank('7')
+  case object Eight extends Rank('8')
+  case object Nine extends Rank('9')
+  case object Ten extends Rank('T')
+  case object Jack extends Rank('J')
+  case object Queen extends Rank('Q')
+  case object King extends Rank('K')
+  case object Ace extends Rank('A')
+
+  val ordered: List[Rank] =
+    Two ::
+    Three ::
+    Four ::
+    Five ::
+    Six ::
+    Seven ::
+    Eight ::
+    Nine ::
+    Ten ::
+    Jack ::
+    Queen ::
+    King ::
+    Ace ::
+    Nil
+
+  def of(x: Char): Either[ErrorMessage, Rank] = {
+    ordered.find(_.value == x).toRight(s"Unrecognized rank $x")
+  }
 }
--- src/main/scala/com/evolution/bootcamp/assignment/poker/Suit.scala
+++ src/main/scala/com/evolution/bootcamp/assignment/poker/Suit.scala
@@ -1,8 +1,33 @@
 package com.evolution.bootcamp.assignment.poker
 
-sealed trait Suit
-// TODO
+import cats.syntax.either._
+
+sealed trait Suit {
+  def character: Char
+}
+
+case object Hearts extends Suit {
+  val character = 'h'
+}
+
+case object Diamonds extends Suit {
+  val character = 'd'
+}
+
+case object Clubs extends Suit {
+  val character = 'c'
+}
+
+case object Spades extends Suit {
+  val character = 's'
+}
 
 object Suit {
-  def of(x: Char): Either[ErrorMessage, Suit] = ???
+  def of(x: Char): Either[ErrorMessage, Suit] = x match {
+    case Hearts.character   => Hearts.asRight
+    case Diamonds.character => Diamonds.asRight
+    case Clubs.character    => Clubs.asRight
+    case Spades.character   => Spades.asRight
+    case _                  => s"Invalid char for suit: $x".asLeft
+  }
 }
