59006ec3bbd77817cb58b171d0e625a8639eefc5
jurisk <jurisk@users.noreply.github.com>
2020-10-27 21:37:44 +0200

    Minor tweaks

--- src/main/scala/com/evolution/bootcamp/assignment/poker/Solution.scala
+++ src/main/scala/com/evolution/bootcamp/assignment/poker/Solution.scala
@@ -3,7 +3,12 @@ package com.evolution.bootcamp.assignment.poker
 final case class Solution(
   groups: List[Set[Hand]],
 ) {
-  override def toString: String = groups.map { set =>
-    set.map(_.toString).toList.sorted.mkString("=")
-  }.mkString(" ")
+  override def toString: String = groups
+    .map {
+      _ .map(_.toString)
+        .toList
+        .sorted
+        .mkString("=")
+    }
+    .mkString(" ")
 }
