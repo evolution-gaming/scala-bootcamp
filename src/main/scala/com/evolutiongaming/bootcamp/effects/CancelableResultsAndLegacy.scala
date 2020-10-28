6d31a078c1660cb1acd1588fffe887d1b909111f
Maris Ruskulis <mruskulis@evolutiongaming.com>
2020-10-28 17:44:43 +0200

    adds thread pool starvation example, refactor

---
+++ src/main/scala/com/evolutiongaming/bootcamp/effects/CancelableResultsAndLegacy.scala
@@ -0,0 +1,69 @@
+package com.evolutiongaming.bootcamp.effects
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import cats.effect.{ExitCode, IO, IOApp}
+import com.evolutiongaming.bootcamp.effects.Console.Real.putStrLn
+import scala.concurrent.duration._
+
+/* Cancellation on legacy code,
+ * When writing your cancellable code, be aware that cancellation is a concurrent action.
+ * That is, there is no synchronization provided by Cats Effect IO for it.
+ * Therefore, if your effect code is doing an operation that isn't safe to do concurrently with cancellation,
+ * it can lead data corruption or other errors.
+ * You can solve it, for example, by introducing a lock, as per Cats Effect IO documentation here: ...
+ * https://typelevel.org/cats-effect/datatypes/io.html#gotcha-cancellation-is-a-concurrent-action
+ */
+object CancelableResultsAndLegacy extends IOApp {
+
+  private val cancelableLegacyIntegrationProgram = {
+    class UglyLegacyCode {
+      private val cancelled = new AtomicBoolean(false)
+
+      private def longRecursiveCompute(x: Long, until: Long): Long = {
+        if (cancelled.get()) {
+          throw new InterruptedException("compute interrupted")
+        } else if (x >= until) {
+          x
+        } else {
+          println(s" ${Thread.currentThread().toString} Calculating in longRecursiveCompute: $x")
+          Thread.sleep(1000)
+          longRecursiveCompute(x + x, until)
+        }
+      }
+
+      def compute(i: Long, until: Long)(onComplete: Long => Unit, onError: Exception => Unit): Unit = {
+        val t = new Thread(() => {
+          try {
+            onComplete(longRecursiveCompute(i, until))
+          } catch {
+            case e: InterruptedException => onError(e)
+          }
+        })
+        t.start()
+      }
+
+      def cancel(): Unit = cancelled.set(true)
+    }
+
+    for {
+      _ <- putStrLn("Launching cancelable")
+      io = IO.cancelable[Long] { cb =>
+        val legacy = new UglyLegacyCode
+        legacy.compute(2L, Long.MaxValue)(res => cb(Right(res)), e => cb(Left(e)))
+        IO.delay(legacy.cancel())
+      }
+      fiber <- io.start
+      _ <- putStrLn(s"Started $fiber")
+      res <- IO.race(IO.sleep(10.seconds), fiber.join)
+      _ <-
+        res.fold(
+          _ => putStrLn(s"cancelling $fiber...") *> fiber.cancel *> putStrLn("IO cancelled"),
+          i => putStrLn(s"IO completed with: $i")
+        )
+    } yield ()
+  }
+  override def run(args: List[String]): IO[ExitCode] = for {
+    _ <- cancelableLegacyIntegrationProgram
+  } yield ExitCode.Success
+}
---
+++ src/main/scala/com/evolutiongaming/bootcamp/effects/ContextShifts.scala
@@ -0,0 +1,121 @@
+package com.evolutiongaming.bootcamp.effects
+
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{Executors, ThreadFactory}
+
+import cats.effect.{Blocker, ContextShift, ExitCode, IO, IOApp, Resource}
+import cats.implicits._
+import com.evolutiongaming.bootcamp.effects.Console.Real.putStrLn
+
+import scala.concurrent.ExecutionContext
+
+/*
+ * `ContextShift` is the pure equivalent to `ExecutionContext`:
+ * - https://typelevel.org/cats-effect/datatypes/contextshift.html
+ *
+ * `ContextSwitch#shift` or `IO.shift` can be used to do "cooperative yielding" by triggering a logical fork
+ * so that the current thread is not occupied on long running operations.
+ *
+ * When choosing sizes for thread pools rule of thumb:
+ * 1. cpu-bound tasks should have not more then available cores on the machine threads, should be used for pure computation only
+ *    IO app default ContextShift constructed as `math.max(2, Runtime.getRuntime().availableProcessors())`
+ * 2. blocking io tasks - unbounded thread pool used only for blocking tasks,
+ *    avoid using for computation as that will lead to poor performance because of context switches
+ * https://typelevel.org/cats-effect/concurrency/basics.html#thread-pools
+ * https://typelevel.org/cats-effect/concurrency/basics.html#concurrency
+ * https://monix.io/docs/3x/best-practices/blocking.html
+ *
+ * Failure to run blocking tasks on separate pool(via blocker) will lead to thread pool starvation. See example.
+ *
+ * Example showcases how does context shift works in terms of executing thread.
+ * 1. cpu bound pool
+ * 2. Blocker https://typelevel.org/cats-effect/datatypes/contextshift.html#blocker
+ */
+object ContextShifts extends IOApp {
+
+  def logLine(s: => String): IO[Unit] = IO.suspend(putStrLn(s"${Thread.currentThread().toString} $s"))
+
+  def newThreadFactory(name: String): ThreadFactory = new ThreadFactory {
+    val ctr = new AtomicInteger(0)
+
+    def newThread(r: Runnable): Thread = {
+      val back = new Thread(r, s"$name-pool-${ctr.getAndIncrement()}")
+      back.setDaemon(true)
+      back
+    }
+  }
+
+  // dedicated pool with 2 threads for cpu bound tasks
+  // io-app default pools size is calculted as math.max(2, Runtime.getRuntime().availableProcessors())
+  // in case we want to restrict certain computation and not to interfere with global pool
+  // abusing thread pools may lead to unnecessary context switches which will degrade performance
+  val basicShiftingExample: IO[Unit] = {
+    val cpuBoundPool: ExecutionContext =
+      ExecutionContext
+        .fromExecutor(Executors.newFixedThreadPool(2, newThreadFactory("cpu-bound")))
+
+    val cpuBoundContext = IO.contextShift(cpuBoundPool)
+
+    def cpuBound(d: Double, invocation: Long): IO[Double] = IO.suspend {
+      if (d == 0.0) IO.pure(d)
+      else cpuBound(d / 2.0, invocation + 1) //putStrLn(s"${Thread.currentThread().toString} current value: ${d}") *>
+    }
+
+    for {
+      _ <- logLine(s"Started on default thread")
+      _ <- ContextShift[IO].evalOn(cpuBoundPool)(logLine(s"Evaling on cpu-bound-pool"))
+      _ <- logLine(s"We are back on main default")
+
+      result <- cpuBoundContext.shift >> logLine(s"running on cpu-bound-pool-") *> cpuBound(100000.0, 0)
+      _ <- IO.shift >> logLine(s"result=${result} result on default")
+    } yield ()
+  }
+
+  // https://typelevel.org/cats-effect/datatypes/contextshift.html#blocker
+  // special pool with explicit construct for blocking operations
+  val blockingExample: IO[Unit] = {
+    val blocker: Resource[IO, Blocker] = Blocker.fromExecutorService(IO.delay(Executors.newCachedThreadPool(newThreadFactory("blocker"))))
+
+    blocker.use { blocker =>
+      def blockingCall(id: Int): Unit = {
+        println(s"${Thread.currentThread().toString} Starting blocking work id:$id")
+        Thread.sleep(5000)
+        println(s"${Thread.currentThread().toString} Ended work id:$id")
+      }
+
+      //launching paralell 10 blocking tasks
+      (0 to 9).toList.map(id => blocker.delay[IO, Unit](blockingCall(id))).parSequence.void
+    }
+  }
+
+  //Anti pattern: running blocking tasks on cpu-bound pool
+  val threadPoolStarvationExample: IO[Unit] = {
+    val twoThreadPool: ExecutionContext =
+      ExecutionContext
+        .fromExecutor(Executors.newFixedThreadPool(2, newThreadFactory("cpu-bound")))
+    val cs = IO.contextShift(twoThreadPool)
+
+    def blockingCall(): Unit = {
+      logLine("Starting blocking call").unsafeRunSync()
+      Thread.sleep(5000)
+      logLine("End blocking call").unsafeRunSync()
+    }
+
+    for {
+      _ <- cs.shift *> logLine("Starting thread pool startvation example")
+      _ <- logLine("Spawning blocking tasks on 2 thread pool")
+      fib <- (0 to 9).toList.map(_ => cs.shift *> IO.delay(blockingCall())).parSequence.void.start
+      _ <- cs.shift *> logLine("This will happen only when thread frees up, such use of cpu bound pool would cause entire program to freeze")
+      _ <- fib.join
+    } yield ()
+  }
+
+  def run(args: List[String]): IO[ExitCode] = {
+    for {
+      _ <- basicShiftingExample
+      _ <- blockingExample
+      _ <- threadPoolStarvationExample
+      _ <- logLine("End")
+    } yield ExitCode.Success
+  }
+}
