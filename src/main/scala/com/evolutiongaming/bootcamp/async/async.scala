0caab733e83f34eeedb5fbe87b3225610efcc415
Mikhail Sokolov <msokolov@evolutiongaming.com>
2020-10-29 13:22:01 +0200

    Add exercise

--- src/main/scala/com/evolutiongaming/bootcamp/async/async.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/async/async.scala
@@ -240,3 +240,46 @@ object SharedStateAtomic extends App {
   Await.ready(thread2, 5.seconds)
   println(counter)
 }
+
+/*
+Make this work correctly a) first with synchronized blocks, b) then with AtomicReference
+ */
+object Exercise3 extends App {
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val tasksCount = 100
+  val taskIterations = 1000
+  val initialBalance = 10
+
+  //PLACE TO FIX - START
+  var balance1: Int = initialBalance
+  var balance2: Int = initialBalance
+
+  def doTaskIteration(): Unit = {
+    val State(newBalance1, newBalance2) = transfer(State(balance1, balance2))
+    balance1 = newBalance1
+    balance2 = newBalance2
+  }
+
+  def printBalancesSum(): Unit = {
+    println(balance1 + balance2)
+  }
+  //PLACE TO FIX - FINISH
+
+  def transfer(state: State): State = {
+    if (state.balance1 >= state.balance2) {
+      State(state.balance1 - 1, state.balance2 + 1)
+    } else {
+      State(state.balance1 + 1, state.balance2 - 1)
+    }
+  }
+
+  val tasks = (1 to tasksCount).toVector.map(_ => Future {
+    (1 to taskIterations).foreach(_ => doTaskIteration())
+  })
+  val tasksResultFuture: Future[Vector[Unit]] = Future.sequence(tasks)
+  Await.ready(tasksResultFuture, 5.seconds)
+
+  printBalancesSum() //should print 20
+
+  final case class State(balance1: Int, balance2: Int)
+}
