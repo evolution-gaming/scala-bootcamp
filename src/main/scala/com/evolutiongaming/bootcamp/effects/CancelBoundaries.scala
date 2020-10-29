8be44412046b2e5f32cc574ee3adecac7610ce6b
Maris Ruskulis <mruskulis@evolutiongaming.com>
2020-10-29 21:41:16 +0200

    adds exercises on cancellable

--- src/main/scala/com/evolutiongaming/bootcamp/effects/CancelBoundaries.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/effects/CancelBoundaries.scala
@@ -1,12 +1,25 @@
 package com.evolutiongaming.bootcamp.effects
 
 import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits.catsSyntaxParallelSequence
 import com.evolutiongaming.bootcamp.effects.Console.Real.putStrLn
 
 import scala.concurrent.duration._
+import scala.util.Random
+import scala.util.control.NonFatal
 
 /*
- * IO is cancellable only on async boundary IO.shift or on IO.cancelBoundary
+ * IO is cancellable only on async boundary IO.shift or on IO.cancelBoundary and after 512 flatMap loop iterations.
+ * Docs states:
+ * >We should also note that flatMap chains are only cancelable only if the chain happens after an asynchronous boundary.
+ * >After an asynchronous boundary, cancellation checks are performed on every N flatMap. The value of N is hardcoded to 512.
+ * which is bit misleading, because cancellation is checked and counter IS reset on async boundary,
+ * but counter is still taken into account even if not crossing async boundaries.
+ * Technically IO is switching from Main to io-app context.
+ * That may lead to inconsistent state when doing `race` with internal state update.
+ * That means - critical block's should be marked as `cancellable`
+ *
+ * https://typelevel.org/cats-effect/datatypes/io.html#concurrency-and-cancellation
  * https://typelevel.org/cats-effect/datatypes/io.html#iocancelboundary
  */
 object CancelBoundaries extends IOApp  {
@@ -52,5 +65,56 @@ object CancelBoundaries extends IOApp  {
   override def run(args: List[String]): IO[ExitCode] = for {
     _ <- cancelableProgram
     _ <- nonCancelableProgram
+  } yield ExitCode.Success
+}
+
+object CancelBoundariesExercises extends IOApp {
+  def delay(duration: FiniteDuration): IO[Unit] = IO.sleep(duration).uncancelable
+
+  /*
+   * Fix retry function without altering delay function, to be cancellable immediately, so running program there is no retrying after cancel
+   */
+  val retryExercise: IO[Unit] = {
+    implicit class ioRetrySyntax[A](task: IO[A]) {
+      def retry(id: String, maxRetries: Int, interval: FiniteDuration): IO[A] =
+        task
+          .handleErrorWith {
+            case NonFatal(e) =>
+              putStrLn(s"$id Retrying... retries left: $maxRetries") *> (if(maxRetries <= 0) IO.raiseError(e)
+              else delay(interval) *> IO.suspend(task.retry(id, maxRetries-1, interval)))
+          }
+    }
+
+    val io = IO.delay(if(Random.nextBoolean()) throw new RuntimeException("kaboom!") else "SUCCESS!")
+    for {
+      fib <- (0 to 10).toList.map(id => io.retry(s"id:$id", 10, 5.second))
+        .parSequence.flatMap(ll => putStrLn(ll.toString())).start
+      _ <- IO.sleep(5.seconds)
+      _ <- fib.cancel
+      _ <- putStrLn("No more work after this point")
+      _ <- IO.sleep(30.seconds)
+      _ <- putStrLn(s"End")
+    } yield ()
+  }
+
+  /*
+   * Fix program so that no Calculation is happening after cancellation
+   */
+  val computeExercise = {
+    def cpuBoundCompute(value: BigInt, multiplier: BigInt): IO[BigInt] = {
+      val log = IO.delay(println(s"${Thread.currentThread().toString} Calculating... ${multiplier}"))
+      log *> IO.suspend(cpuBoundCompute(value * multiplier, multiplier + 1))
+    }
+    for {
+      _ <- putStrLn("Starting program")
+      fib <- cpuBoundCompute(1, 1).start
+      _ <- fib.cancel
+      _ <- putStrLn("cpu bound cancelled")
+      _ <- IO.sleep(10.seconds)
+    } yield ()
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = for {
+    _ <- computeExercise
   } yield ExitCode.Success
 }
