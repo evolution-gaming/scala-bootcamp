1df416639e5553093a03c92b79ba98ff1fe5fa89
Dzmitryi Linou <dlinov@evolutiongaming.com>
2021-03-09 16:51:11 +0300

    Cache some responses for json homework

--- src/test/scala/com/evolutiongaming/bootcamp/json/HomeworkSpec.scala
+++ src/test/scala/com/evolutiongaming/bootcamp/json/HomeworkSpec.scala
@@ -14,6 +14,9 @@ import org.scalatest.matchers.must.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import scalaj.http.Http
 
+import scala.io.Source
+import scala.util.Try
+
 /**
  * HOMEWORK:
  *
@@ -98,13 +101,29 @@ object HomeworkSpec {
 
   private def fetchScoreboard(date: LocalDate): Either[circe.Error, Scoreboard] = {
     val dateString = date.format(DateTimeFormatter.BASIC_ISO_DATE)
-    val body = Http(s"https://data.nba.net/10s/prod/v1/$dateString/scoreboard.json").asString.body
-    decode[Scoreboard](body)
+    val jsonString = Try {
+      val src = Source.fromResource(s"scoreboard_$dateString.json")
+      val s = src.mkString
+      src.close()
+      s
+    }.getOrElse {
+      val url = s"https://data.nba.net/10s/prod/v1/$dateString/scoreboard.json"
+      Http(url).asString.body
+    }
+    decode[Scoreboard](jsonString)
   }
 
   private def fetchGameInfo(date: LocalDate, gameId: String): Either[circe.Error, BoxScore] = {
-    val dateString = date.format(DateTimeFormatter.BASIC_ISO_DATE)
-    val body = Http(s"https://data.nba.net/10s/prod/v1/$dateString/${gameId}_boxscore.json").asString.body
-    decode[BoxScore](body)
+    val jsonString = Try {
+      val src = Source.fromResource(s"${gameId}_boxscore.json")
+      val s = src.mkString
+      src.close()
+      s
+    }.getOrElse {
+      val dateString = date.format(DateTimeFormatter.BASIC_ISO_DATE)
+      val url = s"https://data.nba.net/10s/prod/v1/$dateString/${gameId}_boxscore.json"
+      Http(url).asString.body
+    }
+    decode[BoxScore](jsonString)
   }
 }
