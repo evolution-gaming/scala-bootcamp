8e8eb9c9fd82cfc8ac9217213f19c1c557fdcd33
Evgeny Kurnevsky <kurnevsky@gmail.com>
2021-04-15 16:39:40 +0300

    Move homework to a separate file.

---
+++ src/main/scala/com/evolutiongaming/bootcamp/http/Homework.scala
@@ -0,0 +1,23 @@
+package com.evolutiongaming.bootcamp.http
+
+// Homework. Place the solution under `http` package in your homework repository.
+//
+// Write a server and a client that play a number guessing game together.
+//
+// Communication flow should be as follows:
+// 1. The client asks the server to start a new game by providing the minimum and the maximum number that can
+//    be guessed.
+// 2. The server comes up with some random number within the provided range.
+// 3. The client starts guessing the number. Upon each attempt, the server evaluates the guess and responds to
+//    the client, whether the current number is lower, greater or equal to the guessed one.
+// 4. The game ends when the number is guessed or there are no more attempts left. At this point the client
+//    should terminate, while the server may continue running forever.
+// 5. The server should support playing many separate games (with different clients) at the same time.
+//
+// The exact protocol and message format to use is not specified and should be designed while working on the task.
+object GuessServer {
+  // ...
+}
+object GuessClient {
+  // ...
+}
