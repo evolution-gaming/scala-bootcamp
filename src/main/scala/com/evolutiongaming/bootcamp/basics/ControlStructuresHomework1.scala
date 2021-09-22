0ee9766edf6bb8cf362670cc351114e9f01a6b13
Viktors Nuzdins <vnuzdins@evolution.com>
2021-09-22 14:23:55 +0300

    Improve task descriptions for ControlStructuresHomework1

--- src/main/scala/com/evolutiongaming/bootcamp/basics/ControlStructuresHomework1.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/basics/ControlStructuresHomework1.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object ControlStructuresHomework1 {
   type Error = String
 
-  // consider person an adult if their age is higher than 18
+  // consider person an adult if their age is higher or equal to 18
   // return error message "$age is too high, are you human?" if age is higher than 150
   // return error message "$age is negative, we do not serve unborn people" if age is lower than 0
   // use if-else
@@ -14,7 +14,7 @@ object ControlStructuresHomework1 {
   // same as isAdultIf, but use match statement instead
   def isAdultMatch(age: Int): Either[Error, Boolean] = ???
 
-  // https://en.wikipedia.org/wiki/Triangle_inequality
+  // https://en.wikipedia.org/wiki/Triangle_inequality, consider degenerate triangles invalid
   // can you do it without using any control structures?
   def isValidTriangle(a: Double, b: Double, c: Double): Boolean = ???
 
@@ -25,13 +25,14 @@ object ControlStructuresHomework1 {
   //
   // * Each year of experience gives candidate 1 point, but not more than 5
   // * If candidate has education, it gives him 3 points
-  // * Each passed test gives candidate 1 point after 5 passed tests.
+  // * Each passed test gives candidate 1 point starting from 6th passed test.
   //   If candidate has passed less than 5 tests, they don't want to hire him in any case.
   // * They prefer candidates from their country, so being from Wakanda gives candidate 3 points.
   //   If the candidate is from any of the neighboring countries - Narnia, Skyrim or Amestris -
   //   candidate should get 1 point, otherwise - 0
   // * Summed stars on github also give points.
-  //   1 point if candidate has 10 stars, 2 points for 100, 3 points for 1000 and so on
+  //   1 point if candidate has 10 stars, 2 points for 100, 3 points for 1000 and so on,
+  //   giving 1 point for each WHOLE new power of ten, e.g. 9 is still 0 points, 99 is still 1 point.
   //
   // All input is valid, e.g. candidate can't have negative years of experience
   def isValidCandidate(
