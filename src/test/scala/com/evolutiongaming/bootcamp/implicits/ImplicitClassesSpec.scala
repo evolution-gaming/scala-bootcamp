02204a5a2a60fc7f3bb2ef0fa3c16fafb1e7067f
Ruslans Tarasovs <rtarasov@evolutiongaming.com>
2021-10-12 11:23:49 +0300

    WIP: Implicit classes.

---
+++ src/test/scala/com/evolutiongaming/bootcamp/implicits/ImplicitClassesSpec.scala
@@ -0,0 +1,19 @@
+package com.evolutiongaming.bootcamp.implicits
+
+import org.scalatest.funsuite.AnyFunSuite
+
+import Implicits._
+
+class ImplicitsSpec extends AnyFunSuite {
+
+  test("Excersise 1: pow") {
+    assert(EvolutionUtils0.pow(2, 0) == 1)
+    assert(EvolutionUtils0.pow(2, 1) == 2)
+    assert(EvolutionUtils0.pow(2, 2) == 4)
+    assert(EvolutionUtils0.pow(2, 10) == 1024)
+    assert(EvolutionUtils0.pow(2, -1) == 0)
+    assert(EvolutionUtils0.pow(2, -2) == 0)
+    assert(EvolutionUtils0.pow(3, 2) == 9)
+  }
+
+}
