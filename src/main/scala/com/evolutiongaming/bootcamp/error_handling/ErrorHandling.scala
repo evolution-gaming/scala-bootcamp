87423a951561bd94f76a70062d7f3d3ffec12ab3
Andrejs Pavlovics <apavlovics2@evolution.com>
2021-10-20 10:48:54 +0300

    Extract Error Handling homework to CodeScreen

--- src/main/scala/com/evolutiongaming/bootcamp/error_handling/ErrorHandling.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/error_handling/ErrorHandling.scala
@@ -219,33 +219,6 @@ object ErrorHandling extends App {
   // the same result.
   type FutureEither[E, A] = Future[Either[E, A]]
 
-  // Homework. Place the solution under `error_handling` package in your homework repository.
-  //
-  // 1. Model `PaymentCard` class as an ADT (protect against invalid data as much as it makes sense).
-  // 2. Add `ValidationError` cases (at least 5, may be more).
-  // 3. Implement `validate` method to construct `PaymentCard` instance from the supplied raw data.
-  object Homework {
-
-    case class PaymentCard(/* Add parameters as needed */)
-
-    sealed trait ValidationError
-    object ValidationError {
-      ??? // Add errors as needed
-    }
-
-    object PaymentCardValidator {
-
-      type AllErrorsOr[A] = ValidatedNec[ValidationError, A]
-
-      def validate(
-        name: String,
-        number: String,
-        expirationDate: String,
-        securityCode: String,
-      ): AllErrorsOr[PaymentCard] = ???
-    }
-  }
-
   // Attributions and useful links:
   // https://www.lihaoyi.com/post/StrategicScalaStylePrincipleofLeastPower.html#error-handling
   // https://www.geeksforgeeks.org/scala-exception-handling/
