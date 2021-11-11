f3a11a9f24554f41a86baca74fdfddb92e2bf2a7
Ivan Petrov <twik@bk.ru>
2021-11-11 17:55:31 +0200

    Effects lecture v3

---
+++ src/main/scala/com/evolutiongaming/bootcamp/effects/v3/Cancellation.scala
@@ -0,0 +1,330 @@
+package com.evolutiongaming.bootcamp.effects.v3
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.syntax.all._
+
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicBoolean
+import scala.annotation.tailrec
+import scala.concurrent.duration._
+import scala.concurrent.{Future, blocking}
+import scala.util.Random
+import scala.util.control.NonFatal
+
+/*
+ * Why cancellable IO is needed? Why it's better then Future?
+ * - One cannot just simply cancel Future
+ *   - cannot cancel on error condition
+ *   - cannot cancel on race
+ *
+ * Futures will continue doing their work, spending precious computing resources, till the work is done
+ *
+ * Cancellable IO to the rescue!
+ */
+
+object FutureTimeout extends IOApp {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  def runTask(i: Int): Future[Unit] =
+    Future {
+      blocking {
+        (1 to i).foreach { iteration =>
+          println(s"${Thread.currentThread().toString} Starting iteration - $iteration")
+          Thread.sleep(1000)
+          println(s"${Thread.currentThread().toString} Done iteration - $iteration")
+        }
+      }
+    }
+
+  def completeAfter(timeout: FiniteDuration): Future[Unit] =
+    Future {
+      blocking {
+        Thread.sleep(timeout.toMillis)
+        println(s"${Thread.currentThread().toString} Completing future after $timeout ")
+      }
+    }
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- IO.fromFuture(
+             IO.delay(
+               Future.firstCompletedOf[Unit](
+                 Seq(
+                   runTask(10),
+                   completeAfter(5.seconds)
+                 )
+               )
+             )
+           )
+      //      _ <- IO.sleep(5.seconds)
+    } yield ExitCode.Success
+}
+
+object IOTimeout extends IOApp {
+
+  def runTask(i: Int): IO[Unit] =
+    (1 to i).toList
+      .map { iteration =>
+        for {
+          _ <- IO.delay(println(s"${Thread.currentThread().toString} Starting iteration - $iteration"))
+          _ <- IO.sleep(1.second)
+          _ <- IO.delay(println(s"${Thread.currentThread().toString} Done iteration - $iteration"))
+        } yield ()
+      }
+      .sequence
+      .void
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- runTask(10).timeout(5.seconds).attempt
+      _ <- IO.delay(println(s"${Thread.currentThread().toString} Cancelled"))
+      //      _ <- IO.sleep(5.seconds)
+    } yield ExitCode.Success
+}
+
+object RaceApp extends IOApp {
+
+  val tick: IO[Unit] =
+    IO
+      .delay(println("Working work long long never terminating"))
+      .flatMap(_ => IO.sleep(1.second))
+      .foreverM
+      .void
+
+  val workThatDoesFaster: IO[Unit] = IO.sleep(5.seconds) *> IO.delay(println("Work has been done"))
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- IO.race(tick, workThatDoesFaster).void
+      //      _ <- IO.sleep(5.seconds)
+      _ <- IO.delay(println("Terminating"))
+    } yield ExitCode.Success
+}
+
+object SelfMadeTimeoutExercise extends IOApp {
+
+  val tick: IO[Unit] =
+    IO
+      .delay(println("Working work long long never terminating"))
+      .flatMap(_ => IO.sleep(1.second))
+      .foreverM
+      .void
+
+  def timeoutIO[A](task: IO[A], timeout: FiniteDuration): IO[A] = ???
+
+  def run(args: List[String]): IO[ExitCode] =
+    timeoutIO(tick, 5.seconds).as(ExitCode.Success)
+}
+
+/* When writing your cancellable code, be aware that cancellation is a concurrent action.
+ * That is, there is no synchronization provided by Cats Effect IO for it.
+ * Therefore, if your effect code is doing an operation that isn't safe to do concurrently with cancellation,
+ * it can lead data corruption or other errors.
+ * You can solve it, for example, by introducing a lock, as per Cats Effect IO documentation here:
+ * https://typelevel.org/cats-effect/docs/2.x/datatypes/io#gotcha-cancellation-is-a-concurrent-action
+ */
+object Cancellable extends IOApp {
+
+  private val ec = Executors.newFixedThreadPool(4)
+
+  class LegacyCode {
+
+    private val cancelled = new AtomicBoolean(false)
+
+    @tailrec
+    private def longRecursiveCompute(x: Long, until: Long): Long =
+      if (cancelled.get())
+        throw new InterruptedException("longRecursiveCompute interrupted")
+      else if (x >= until)
+        x
+      else {
+        println(s" ${Thread.currentThread().getName} Calculating in longRecursiveCompute: $x")
+        Thread.sleep(1000)
+        longRecursiveCompute(x + x, until)
+      }
+
+    def compute(i: Long, until: Long)(onComplete: Long => Unit, onError: Exception => Unit): Unit = {
+      val t = new Thread(() =>
+        try onComplete(longRecursiveCompute(i, until))
+        catch {
+          case e: InterruptedException => onError(e)
+        }
+      )
+      ec.execute(t)
+    }
+
+    def cancel(): Unit = cancelled.set(true)
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+
+    val program = for {
+      _     <- IO.delay(println("Launching cancelable"))
+      io     = IO.cancelable[Long] { cb =>
+                 val legacy = new LegacyCode
+                 legacy.compute(2L, Long.MaxValue)(res => cb(Right(res)), e => cb(Left(e)))
+//                 legacy.compute(2L, 100)(res => cb(Right(res)), e => cb(Left(e)))
+                 IO.delay(legacy.cancel())
+               }
+      fiber <- io.start
+      _     <- IO.delay(println(s"Started $fiber"))
+      res   <- IO.race(IO.sleep(10.seconds), fiber.join)
+      _     <- res.fold(
+                 _ => IO.delay(println(s"cancelling $fiber...")) *> fiber.cancel *> IO.delay(println("IO cancelled")),
+                 i => IO.delay(println(s"IO completed with: $i"))
+               )
+    } yield ExitCode.Success
+
+    program.guarantee(IO(ec.shutdown()))
+  }
+}
+
+/*
+ * IO is cancellable only on async boundary `IO.shift` or on `IO.cancelBoundary` and after 512 flatMap loop iterations.
+ * Documentation states:
+ *
+ *   We should also note that flatMap chains are only cancelable only if the chain happens after an asynchronous
+ *   boundary.
+ *
+ *   After an asynchronous boundary, cancellation checks are performed on every N flatMap. The value of N is hardcoded to
+ *   512.
+ *
+ * This is bit misleading, because cancellation is checked and counter IS reset on async boundary,
+ * but the counter is still taken into account even if not crossing async boundaries.
+ *
+ * Technically IO is switching from Main to io-app context.
+ *
+ * That may lead to inconsistent state when doing `race` with internal state update.
+ *
+ * That means - critical blocks should be marked as `uncancellable`
+ *
+ * https://typelevel.org/cats-effect/docs/2.x/datatypes/io#concurrency-and-cancellation
+ * https://typelevel.org/cats-effect/docs/2.x/datatypes/io#iocancelboundary
+ */
+object CancelBoundaries extends IOApp {
+
+  val nonCancelableProgram: IO[Unit] = {
+
+    //program has no context shift and no cancel boundary set, it's not cancellable
+    def nonCancellableTimes(rec: Int): IO[Unit] =
+      for {
+        _ <- IO.delay(println(s"Running remaining iterations: $rec"))
+        _ <- IO.sleep(1.seconds).uncancelable
+        _ <- if (rec > 0) IO.suspend(nonCancellableTimes(rec - 1)) else IO.unit
+      } yield ()
+
+    for {
+      _   <- IO.delay(println("Starting nonCancelableProgram"))
+      fib <- nonCancellableTimes(10).start
+      _   <- IO.sleep(5.seconds)
+      _   <- fib.cancel
+      _   <- IO.delay(println("Cancelled nonCancelableProgram"))
+      _   <- IO.sleep(5.seconds) //just to keep program alive
+      _   <- IO.delay(println("End nonCancelableProgram"))
+    } yield ()
+  }
+
+  val cancelableProgram: IO[Unit] = {
+
+    //on every iteration cancel boundary is set, program is cancellable
+    def cancellableTimes(rec: Int): IO[Unit] =
+      for {
+        _ <- IO.delay(println(s"Running remaining iterations: $rec"))
+        _ <- IO.sleep(1.seconds).uncancelable
+        _ <- if (rec > 0) IO.cancelBoundary *> IO.suspend(cancellableTimes(rec - 1)) else IO.unit
+      } yield ()
+
+    for {
+      _   <- IO.delay(println("Starting cancelableProgram"))
+      fib <- cancellableTimes(10).start
+      _   <- IO.sleep(5.seconds)
+      _   <- fib.cancel
+      _   <- IO.delay(println("Cancelled cancelableProgram"))
+      _   <- IO.sleep(5.seconds) //just to keep program alive
+      _   <- IO.delay(println("End cancelableProgram"))
+    } yield ()
+  }
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- cancelableProgram
+      _ <- nonCancelableProgram
+    } yield ExitCode.Success
+}
+
+object CancelBoundariesExercise extends IOApp {
+
+  def delay(duration: FiniteDuration): IO[Unit] = IO.sleep(duration).uncancelable
+
+  /* Exercise #1
+   * Fix retry function without altering delay function, to be cancellable immediately, so that running the program
+   * there is no retrying after cancel
+   */
+  val retryExercise: IO[Unit] = {
+    implicit class ioRetrySyntax[A](task: IO[A]) {
+      def retry(
+        id: String,
+        maxRetries: Int,
+        interval: FiniteDuration
+      ): IO[A] =
+        task
+          .handleErrorWith {
+            case NonFatal(e) =>
+              IO
+                .delay(println(s"$id Retrying... retries left: $maxRetries"))
+                .flatMap { _ =>
+                  if (maxRetries <= 0) IO.raiseError(e)
+                  else
+                    delay(interval) *> IO.suspend(
+                      task.retry(
+                        id,
+                        maxRetries - 1,
+                        interval
+                      )
+                    )
+                }
+          }
+    }
+
+    val io = IO.delay(if (Random.nextBoolean()) throw new RuntimeException("kaboom!") else "SUCCESS!")
+    for {
+      fib <- (0 to 10).toList
+               .map(id => io.retry(s"id:$id", 10, 5.second))
+               .parSequence
+               .flatMap(ll => IO.delay(println(ll.toString())))
+               .start
+      _   <- IO.sleep(5.seconds)
+      _   <- fib.cancel
+      _   <- IO.delay(println("No more work after this point"))
+      _   <- IO.sleep(10.seconds)
+      _   <- IO.delay(println("End"))
+    } yield ()
+  }
+
+  /* Exercise #2
+   * Fix program so that no Calculation is happening after cancellation
+   */
+  val computeExercise: IO[Unit] = {
+
+    def cpuBoundCompute(value: BigInt, multiplier: BigInt): IO[BigInt] = {
+      val log = IO.delay(println(s"${Thread.currentThread().toString} Calculating... $multiplier"))
+      log *> IO.suspend(cpuBoundCompute(value * multiplier, multiplier + 1))
+    }
+
+    for {
+      _   <- IO.delay(println("Starting program"))
+      fib <- cpuBoundCompute(1, 1).start
+      _   <- fib.cancel
+      _   <- IO.delay(println("cpuBoundCompute cancelled"))
+      _   <- IO.sleep(10.seconds)
+      _   <- IO.delay(println("End"))
+    } yield ()
+  }
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- retryExercise
+      _ <- computeExercise
+    } yield ExitCode.Success
+}
---
+++ src/main/scala/com/evolutiongaming/bootcamp/effects/v3/Effects.scala
@@ -0,0 +1,461 @@
+package com.evolutiongaming.bootcamp.effects.v3
+
+import cats.Monad
+import cats.effect.{Async, ExitCode, IO, IOApp, Sync}
+import cats.syntax.all._
+
+import java.util.concurrent.Executors
+import scala.annotation.tailrec
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+import scala.io.StdIn
+import scala.util.control.NoStackTrace
+import scala.util.{Random, Try}
+
+/*
+ * Side effects - Modifying or accessing shared state outside the local environment - producing an
+ *                observable effect other than returning a value to the invoker.
+ *
+ *                For example - updating a global variable, reading/writing to disk/console.
+ *
+ * But don't we need to do these "side effects" to write useful programs?
+ *
+ * Yes, indeed we do.
+ *
+ * In functional programming we wrap side effects into "IO Monads":
+ *  - This turns (or captures, or encodes) them into immutable data (pure values)
+ *  - Keeps referential transparency so that it is easier to refactor our programs and reason about them
+ *  - We can evaluate them when we want
+ *  - They can be executed sequentially or in parallel
+ *  - They can be used in `for`-comprehensions
+ *  - They can model complex series of concurrent computations
+ *
+ * There are a number of popular libraries providing IO Monads in the Scala ecosystem, each with their
+ * pros & cons:
+ *  - Cats Effect - https://typelevel.org/cats-effect/ - versions 2 and 3
+ *  - ZIO - https://zio.dev/
+ *  - Monix - https://monix.io/
+ *
+ *  There are also ways how to write software without being tied to a particular IO Monad ("Tagless Final"
+ *  pattern)
+ *
+ * Asynchronous Effects, as opposed to Scala `Future`-s, are lazy. Nothing is run until an "unsafe" method
+ * is executed (by your code, or by the `IOApp` trait) - usually at the "end of the world".
+ *
+ * The IO Monad in Cats Effect is called `IO`.
+ *
+ * A value of type IO[A] is a computation which, when evaluated, can perform effects before returning a value
+ * of type A.
+ *
+ * It is a data structure that represents a description of a side effecting computation.
+ *
+ * It describes synchronous or asynchronous computations that:
+ * - On evaluation yield exactly one result
+ * - Can end in either success or failure (and in case of failure, the `flatMap` chains get short-circuited)
+ * - Can be canceled (if the user provides cancellation logic)
+ */
+
+/*
+ * Before running this program, try to guess what the output will be.
+ */
+object LazyVsEagerApp extends App {
+  import concurrent.ExecutionContext.Implicits.global
+
+  private def doTaskFuture(x: String): Future[Unit] =
+    Future { println(s"Doing task $x") }
+
+  val future1: Future[Unit] = for {
+    _ <- doTaskFuture("future1")
+    _ <- doTaskFuture("future1")
+  } yield ()
+
+  Await.result(future1, Duration.Inf)
+
+  println("-" * 100)
+
+  val taskFuture            = doTaskFuture("future2")
+  val future2: Future[Unit] = for {
+    _ <- taskFuture
+    _ <- taskFuture
+  } yield ()
+
+  Await.result(future2, Duration.Inf)
+
+  println("-" * 100)
+
+  private def doTaskIO(x: String): IO[Unit] =
+    IO { println(s"Doing task $x") }
+
+  val io1 = for {
+    _ <- doTaskIO("io1")
+    _ <- doTaskIO("io1")
+  } yield ()
+
+  io1.unsafeRunSync()
+
+  println("-" * 100)
+
+  val task = doTaskIO("io2")
+  val io2  = for {
+    _ <- task
+    _ <- task
+  } yield ()
+
+  io2.unsafeRunSync()
+}
+/*
+ * Question. Which approach will be easier to refactor and reason about?
+ *
+ * Question. Can you explain the concept of "referential transparency" using the code above?
+ */
+
+object ConsoleSimple {
+  def readString(): String          = StdIn.readLine()
+  def putString(text: String): Unit = println(text)
+}
+
+object HelloWorldSimple extends App {
+  import ConsoleSimple._
+
+  putString("What is your name: ")
+  val name = readString()
+  putString(s"Hello, $name!\n")
+}
+
+/* Some basic building blocks:
+ *
+ * `IO.pure` lifts pure values into IO, yielding IO values that are "already evaluated".
+ *
+ * It's eagerly evaluated therefore don't pass side effecting computations into it.
+ *
+ * `IO.unit` is just `IO.pure(())`, commonly used to signal completion of side effecting routines.
+ *
+ * `IO.apply` delays the execution of the passed computation to some time in future
+ */
+
+trait Console {
+  def putString(value: String): IO[Unit]
+  def readString: IO[String]
+}
+
+object ConsoleIO extends Console {
+  def putString(value: String): IO[Unit] = IO(println(value))
+  def readString: IO[String]             = IO(StdIn.readLine())
+}
+
+import com.evolutiongaming.bootcamp.effects.v3.ConsoleIO._
+
+/*
+ * `IO` is a Monad and thus you can work with it as you would with other Monad-s, for example,
+ * use `.map`, `.flatMap`, and `for`-comprehensions.
+ *
+ * `IOApp` is the `App` equivalent for `IO`-based programs and serves as the "end of the world" where
+ * effects are run.
+ */
+object HelloWorldIO extends IOApp {
+
+  private val nameProgram = for {
+    _    <- putString("What's your name?")
+    name <- readString
+    _    <- putString(s"Hi, $name!")
+  } yield ()
+
+  def run(args: List[String]): IO[ExitCode] =
+    nameProgram.as(ExitCode.Success)
+}
+
+object HelloWorldTF extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = ???
+}
+
+object Exercise1_Common {
+  def response(animal: String): Option[String] =
+    animal.trim match {
+      case "cat" | "cats"  => "In ancient times cats were worshipped as gods; they have not forgotten this.".some
+      case "dog" | "dogs"  => "Be the person your dog thinks you are.".some
+      case x if x.nonEmpty => s"I don't know what to say about '$x'.".some
+      case _               => none
+    }
+}
+
+object Exercise1_Imperative {
+  import com.evolutiongaming.bootcamp.effects.Exercise1_Common.response
+
+  private var counter: Int = 0
+
+  @tailrec
+  def main(args: Array[String]): Unit = {
+    println("What is your favourite animal?")
+    val animal = StdIn.readLine()
+    val output = response(animal)
+    output match {
+      case Some(x) =>
+        println(x)
+
+      case None    =>
+        if (counter >= 2) {
+          println("I am disappointed. You have failed to answer too many times.")
+          sys.exit(1)
+        } else {
+          counter += 1
+          println("Empty input is not valid, try again...")
+          main(args)
+        }
+    }
+  }
+
+  // Question: How do you test this?
+}
+
+/*
+ * Exercise 1. Re-implement Exercise1_Imperative avoiding side-effecting code using the "IO Monad"
+ *
+ * Using the following can be helpful:
+ *  - `for`-comprehension
+ *  - `IO#as` as a `map` which discards the first result to return `ExitCode`-s
+ *  - `*>` as a `flatMap` which discards the first result to sequence `IO[Unit]` with another `IO`
+ *  - Tests in `EffectsSpec` to check your work
+ */
+object Exercise1_Functional extends IOApp {
+
+  def process(console: Console, counter: Int = 0): IO[ExitCode] = ???
+
+  def run(args: List[String]): IO[ExitCode] = process(ConsoleIO)
+}
+
+/*
+ * `IO.suspend` is equivalent to `IO(f).flatten` and can be used to avoid a stack overflow.
+ *
+ *   def suspend[A](thunk: => IO[A]): IO[A]
+ *
+ * `IO.flatMap` is also "trampolined" (that means - it is stack-safe).
+ *
+ * Question: What happens when `fib` is executed with a large enough `n`?
+ * Question: How can we fix it using `IO.suspend`?
+ */
+object SuspendApp extends IOApp {
+
+  private def fib(
+    n: Int,
+    a: Long = 0,
+    b: Long = 1
+  ): IO[Long] =
+    n match {
+      case 0 => IO.pure(a)
+      case _ => fib(n - 1, b, a + b).map(_ + 0) // Question: Why did I add this useless `.map` here?
+    }
+
+  def run(args: List[String]): IO[ExitCode] =
+    fib(100000)
+      .flatMap(x => putString(s"fib = $x"))
+      .as(ExitCode.Success)
+}
+
+/*
+ * `sequence`     -   takes a list of `IO`, executes them in sequence and returns an `IO` with a collection
+ *                    of all the results.
+ *
+ * `parSequence`  -   does the same, but executes in parallel
+ */
+object Sequence extends IOApp {
+
+  private val listOfInts = (1 to 10).toList
+
+  def printWithDelayAndReturn[A](a: A): IO[A] =
+    IO
+      .sleep(Random.nextInt(1000).millis)
+      .flatMap(_ => putString(a.toString))
+      .as(a)
+
+  private val tasks: List[IO[Int]] =
+    listOfInts.map(printWithDelayAndReturn)
+
+  private val sequenceProgram: IO[Unit] =
+    for {
+      _         <- putString("start sequence")
+      sequenced <- tasks.sequence
+      _         <- putString(s"end sequence, results: $sequenced")
+    } yield ()
+
+  private val parSequenceProgram: IO[Unit] =
+    for {
+      _         <- putString("start parSequence")
+      sequenced <- tasks.parUnorderedSequence
+      _         <- putString(s"end parSequence, results: $sequenced")
+    } yield ()
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      _ <- sequenceProgram
+      _ <- parSequenceProgram
+    } yield ExitCode.Success
+}
+
+/*
+ * Handling errors - operations available for `MonadError` and `ApplicativeError` are available for `IO`.
+ *
+ * See:
+ *  - https://typelevel.org/cats/api/cats/MonadError.html
+ */
+object ErrorsHandling extends IOApp {
+
+  private def failingProgram: IO[String] =
+    for {
+      // `raiseError` fails the `IO` with the specified exception
+      _ <- IO.raiseError { new RuntimeException("errorMessage") }
+    } yield "success"
+
+  def run(args: List[String]): IO[ExitCode] =
+    for {
+      attempt <- failingProgram.attempt // Either[Throwable, A]
+      _       <- putString(s"attempt = $attempt")
+
+      option <- failingProgram.option // Option[A]
+      _      <- putString(s"option = $option")
+
+      handleError <- failingProgram.handleError(x => s"error:  ${x.getMessage}")
+      _           <- putString(s"handleError = $handleError")
+
+      handleErrorWith <- failingProgram.handleErrorWith(x => IO.pure(s"error: ${x.getMessage}"))
+      _               <- putString(s"handleErrorWith = $handleErrorWith")
+
+      recover <- failingProgram.recover {
+                   case x if x.getMessage == "errorMessage" => s"error: ${x.getMessage}"
+                 }
+      _       <- putString(s"recover = $recover")
+
+      recoverWith <- failingProgram.recoverWith {
+                       case x if x.getMessage == "errorMessage" => IO.pure(s"error: ${x.getMessage}")
+                     }
+      _           <- putString(s"recoverWith = $recoverWith")
+
+      redeem <- failingProgram.redeem(
+                  (x: Throwable) => s"error: ${x.getMessage}",
+                  (x: String) => s"success: $x"
+                )
+      _      <- putString(s"redeem = $redeem")
+
+      redeemWith <- failingProgram.redeemWith(
+                      (x: Throwable) => IO.pure(s"error: ${x.getMessage}"),
+                      (x: String) => IO.pure(s"success: $x")
+                    )
+      _          <- putString(s"redeemWith = $redeemWith")
+    } yield ExitCode.Success
+}
+
+object ErrorsExercise extends IOApp {
+
+  trait Console[F[_]] {
+    def readString: F[String]
+    def putString(text: String): F[Unit]
+  }
+
+  object Console {
+    def apply[F[_]: Sync]: Console[F] =
+      new Console[F] {
+        def readString: F[String]            = Sync[F].delay(StdIn.readLine())
+        def putString(text: String): F[Unit] = Sync[F].delay(println(text))
+      }
+  }
+
+  sealed trait ValidationError extends Throwable with NoStackTrace
+
+  object ValidationError {
+
+    final case class InvalidAge(age: Int) extends ValidationError {
+      override def getMessage: String = s"'$age' is invalid age"
+    }
+
+    final case class InvalidName(name: String) extends ValidationError {
+      override def getMessage: String = s"'$name' is invalid name"
+    }
+  }
+
+  sealed abstract case class Age private (value: Int) {
+    override def toString: String = value.toString
+  }
+
+  object Age {
+    def from(value: Int): Either[ValidationError, Age] =
+      Either.cond(value > 0, new Age(value) {}, ValidationError.InvalidAge(value))
+  }
+
+  sealed abstract case class Name private (value: String) {
+    override def toString: String = value
+  }
+
+  object Name {
+    def from(value: String): Either[ValidationError, Name] =
+      Either.cond(value.nonEmpty, new Name(value) {}, ValidationError.InvalidName(value))
+  }
+
+  final case class Person(name: Name, age: Age) {
+    override def toString: String = s"This is $name and he is $age years old"
+  }
+
+  def readPerson[F[_]](console: Console[F]): F[Person] = {
+
+    val readName: F[Name] = ???
+
+    val readAge: F[Age] = ???
+
+    ???
+  }
+
+  def program[F[_]: Monad](console: Console[F]): F[Unit] = ??? // read person and print it
+
+  def run(args: List[String]): IO[ExitCode] =
+    program(Console[IO]).as(ExitCode.Success)
+}
+
+/* `IO` can describe asynchronous processes via the `IO.async` and `IO.cancelable` builders.
+ *
+ * Asynchronous process - a process which continues its execution in a different place or time than the one
+ * that started it.
+ *
+ * Concurrency - a program structuring technique in which there are multiple logical threads of control,
+ * whose effects are interleaved.
+ *
+ * `IO.async` can describe simple asynchronous processes that cannot be canceled.
+ * */
+object AsyncApp extends IOApp {
+
+  private val ec = Executors.newFixedThreadPool(4)
+
+  def request(url: String, callback: Either[Throwable, Int] => Unit): Unit = {
+    val thread = new Thread() {
+      override def run(): Unit = {
+        println(s"Thread from pool: ${Thread.currentThread().getName}")
+        val status = Try(requests.get(url).statusCode)
+        callback(status.toEither)
+      }
+    }
+
+    ec.execute(thread)
+  }
+
+  def requestF[F[_]: Async](url: String): F[Int] =
+    Async[F].async { cb =>
+      println(s"Starting async: ${Thread.currentThread().getName}")
+      val status = Try(requests.get(url).statusCode)
+      cb(status.toEither)
+//      request(url, cb)
+    }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val app = for {
+      response <- IO(StdIn.readLine()) >>= requestF[IO]
+      _        <- IO(println(s"Finished: ${Thread.currentThread().getName}"))
+      _        <- IO(println(response))
+    } yield ()
+
+    app.guarantee(IO(ec.shutdown())) as ExitCode.Success
+  }
+}
+
+/**
+  * `IO.never` represents a non-terminating `IO`
+  */
+object Never extends IOApp {
+  def run(args: List[String]): IO[ExitCode] = IO.never
+}
