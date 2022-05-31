f9fc0c0306bb25cda497567c01017481f794c020
Ivan Petrov <ipetrov@evolution.com>
2022-05-31 21:40:14 +0300

    TF lecture 31.05

--- src/main/scala/com/evolutiongaming/bootcamp/tf/v2/FinalEncoding.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/tf/v2/FinalEncoding.scala
@@ -1,5 +1,135 @@
 package com.evolutiongaming.bootcamp.tf.v2
 
+import cats.{Applicative, Apply, Functor, Id, MonadError, Semigroupal}
+import cats.syntax.all._
+
 object FinalEncoding extends App {
 
+  trait ExpressionA[F[_], A] {
+    def const(n: Int): F[A]
+    def negate(a: F[A]): F[A]
+    def add(a1: F[A], a2: F[A]): F[A]
+  }
+
+  // 13 + (-(1 + 3))
+  def program1[F[_], A](interpreter: ExpressionA[F, A]): F[A] = {
+    import interpreter._
+    add(
+      const(13),
+      negate(add(const(1), const(3)))
+    )
+  }
+
+  def intInterpreter[F[_]: Applicative]: ExpressionA[F, Int] =
+    new ExpressionA[F, Int] {
+      override def const(n: Int): F[Int]               = n.pure[F]
+      override def negate(a: F[Int]): F[Int]           = a.map(a => -a)
+      override def add(a1: F[Int], a2: F[Int]): F[Int] = (a1, a2).mapN { _ + _ }
+    }
+
+  def stringInterpreter[F[_]: Applicative]: ExpressionA[F, String] =
+    new ExpressionA[F, String] {
+      override def const(n: Int): F[String]                     = s"$n".pure[F]
+      override def negate(a: F[String]): F[String]              = a.map(a => s"(-$a)")
+      override def add(a1: F[String], a2: F[String]): F[String] =
+        (a1, a2).mapN((a1, a2) => s"($a1 + $a2)")
+    }
+
+  trait Multiplication[F[_], A] {
+    def multiply(a1: F[A], a2: F[A]): F[A]
+  }
+
+  def intMultiplicationAlgebra[F[_]: Functor: Semigroupal]: Multiplication[F, Int] =
+    new Multiplication[F, Int] {
+      override def multiply(a1: F[Int], a2: F[Int]): F[Int] =
+        (a1, a2).mapN((a1, a2) => a1 * a2)
+    }
+
+  def stringMultiplicationAlgebra[F[_]: Apply]: Multiplication[F, String] =
+    new Multiplication[F, String] {
+      override def multiply(a1: F[String], a2: F[String]): F[String] =
+        (a1, a2).mapN { case (a1, a2) => s"($a1 * $a2)" }
+    }
+
+  // (13 * 2) + (-(1 + 3))
+  def program2[F[_], A](expression: ExpressionA[F, A], multiplication: Multiplication[F, A]): F[A] = {
+    import expression._
+    import multiplication._
+    add(
+      multiply(const(13), const(2)),
+      negate(add(const(1), const(3)))
+    )
+  }
+
+  trait Division[F[_], A] {
+    def divide(a1: F[A], a2: F[A]): F[A]
+  }
+
+  def intDivision[F[_]: MonadError[*[_], String]]: Division[F, Int] =
+    new Division[F, Int] {
+      override def divide(a1: F[Int], a2: F[Int]): F[Int] =
+        (a1, a2).tupled.flatMap {
+          case (_, 0)   => "Division by zero".raiseError[F, Int]
+          case (a1, a2) =>
+            if (a1 % a2 == 0) (a1 / a2).pure[F]
+            else "Division ended up with a modulo".raiseError[F, Int]
+        }
+    }
+
+  def stringDivision[F[_]: Apply]: Division[F, String] =
+    new Division[F, String] {
+      override def divide(a1: F[String], a2: F[String]): F[String] =
+        (a1, a2).mapN { case (a1, a2) => s"($a1 * $a2)" }
+    }
+
+  // ((13 * 2) + (-(1 + 3))) / 2
+  def program3[F[_], A](
+    expression: ExpressionA[F, A],
+    multiplication: Multiplication[F, A],
+    division: Division[F, A]
+  ): F[A] = {
+    import expression._
+    import multiplication._
+    import division._
+    divide(
+      add(
+        multiply(const(13), const(2)),
+        negate(add(const(1), const(3)))
+      ),
+      const(0)
+    )
+  }
+
+  // (13 * 2) + (-((1 + 3) / 2))
+  def program4[F[_], A](
+    expression: ExpressionA[F, A],
+    multiplication: Multiplication[F, A],
+    division: Division[F, A]
+  ): F[A] = {
+    import expression._
+    import multiplication._
+    import division._
+    add(
+      multiply(const(13), const(2)),
+      negate(divide(add(const(1), const(3)), const(2)))
+    )
+  }
+
+  println(program1[Id, String](stringInterpreter))
+  println(program1[Id, Int](intInterpreter))
+
+  println("*" * 100)
+
+  println(program2[Option, String](stringInterpreter, stringMultiplicationAlgebra))
+  println(program2[Id, Int](intInterpreter, intMultiplicationAlgebra))
+
+  println("*" * 100)
+
+  println(program3[Id, String](stringInterpreter, stringMultiplicationAlgebra, stringDivision))
+  println(program3[Either[String, *], Int](intInterpreter, intMultiplicationAlgebra, intDivision))
+
+  println("*" * 100)
+
+  println(program4[Id, String](stringInterpreter, stringMultiplicationAlgebra, stringDivision))
+  println(program4[Either[String, *], Int](intInterpreter, intMultiplicationAlgebra, intDivision))
 }
--- src/main/scala/com/evolutiongaming/bootcamp/tf/v2/WarehouseExample.scala
+++ src/main/scala/com/evolutiongaming/bootcamp/tf/v2/WarehouseExample.scala
@@ -1,7 +1,13 @@
 package com.evolutiongaming.bootcamp.tf.v2
 
+import cats.{Functor, Monad}
+import cats.effect.{IO, Sync}
+import cats.effect.concurrent.Ref
+import cats.syntax.all._
 import com.evolutiongaming.bootcamp.tf.v2.Items.ValidationError.{EmptyName, NegativePrice}
 import com.evolutiongaming.bootcamp.tf.v2.Items.{Item, ValidationError}
+
+import scala.io.StdIn
 
 trait Items[F[_]] {
   def all: F[Map[Long, Item]]
@@ -31,6 +37,49 @@ object Items {
       name  <- Either.cond(name.nonEmpty, name, EmptyName)
       price <- Either.cond(price > 0, price, NegativePrice)
     } yield (name, price)
+
+//  def apply[F[_]]: Interpreter
+//  def of[F[_]]: F[Interpreter]
+//  def make[F[_]]: Resource[F, Interpreter]
+
+  def of[F[_]: Sync]: F[Items[F]] =
+    for {
+      counter <- Ref.of[F, Long](0L)
+      items   <- Ref.of[F, Map[Long, Item]](Map.empty)
+    } yield new ItemsImpl(counter, items)
+
+  private final class ItemsImpl[F[_]: Monad](
+    counter: Ref[F, Long],
+    items: Ref[F, Map[Long, Item]]
+  ) extends Items[F] {
+
+    def all: F[Map[Long, Item]] = items.get
+
+    def create(name: String, price: BigDecimal): F[Either[ValidationError, Item]] =
+      Items.validate(name, price).traverse {
+        case (name, price) =>
+          for {
+            id  <- counter.updateAndGet(_ + 1)
+            item = Item(id, name, price)
+            _   <- items.update(_.updated(id, item))
+          } yield item
+      }
+
+    def update(item: Item): F[Either[ValidationError, Boolean]] =
+      Items.validate(item.name, item.price).traverse { _ =>
+        items.modify { items =>
+          if (items.contains(item.id)) items.updated(item.id, item) -> true
+          else items                                                -> false
+        }
+      }
+
+    def find(id: Long): F[Option[Item]] = items.get.map(_.get(id))
+
+    def delete(id: Long): F[Boolean] =
+      items.modify { items =>
+        (items.removed(id), items.contains(id))
+      }
+  }
 }
 
 trait Console[F[_]] {
@@ -39,4 +88,50 @@ trait Console[F[_]] {
   def putStrLn(str: String): F[Unit]
 }
 
-object Warehouse extends App {}
+object Console {
+
+  def apply[F[_]: Sync]: Console[F] =
+    new Console[F] {
+
+      def readStr: F[String] = Sync[F].delay(StdIn.readLine())
+
+      def readBigDecimal: F[BigDecimal] =
+        readStr.flatMap { str =>
+          try BigDecimal(str).pure[F]
+          catch {
+            case _: Throwable => putStrLn(s"$str is not a BigDecimal") *> readBigDecimal
+//            case _: Throwable =>
+//              putStrLn(s"$str is not a BigDecimal")
+//                .flatMap(_ => readBigDecimal)
+          }
+        }
+
+      def putStrLn(str: String): F[Unit] = Sync[F].delay(println(str))
+    }
+}
+
+object Warehouse extends App {
+
+  def program[F[_]: Monad](console: Console[F], items: Items[F]): F[Either[ValidationError, Item]] =
+    for {
+      name  <- console.readStr
+      price <- console.readBigDecimal
+      item  <- items.create(name, price)
+    } yield item
+
+  def iteration[F[_]: Monad](console: Console[F], items: Items[F]): F[Unit] =
+    for {
+      result <- program(console, items)
+      _      <- console.putStrLn(result.toString)
+      _      <- iteration(console, items)
+    } yield ()
+
+  val test: IO[Unit] =
+    for {
+      items  <- Items.of[IO]
+      console = Console[IO]
+      _      <- iteration(console, items)
+    } yield ()
+
+  test.unsafeRunSync()
+}
